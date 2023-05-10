5d73377c690d67d3bac91ccb77ae2f50be19eb39
Recyclingbottle <hoholeo382@gmail.com>
2023-05-10 15:32:35 +0900

    0510, 병원 찾기 UI 재배치

--- lib/hospital_search.dart
+++ lib/hospital_search.dart
@@ -6,7 +6,6 @@ import 'package:http/http.dart' as http;
 import 'package:location/location.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-
 class HospitalSearchPage extends StatefulWidget {
   final String recommendedSpecialistName;
 
@@ -47,12 +46,19 @@ class _HospitalSearchPageState extends State<HospitalSearchPage> {
         return;
       }
     }
-
     _locationData = await location.getLocation();
-    _currentPosition = CameraPosition(
-      target: LatLng(_locationData!.latitude!, _locationData!.longitude!),
-      zoom: 15.0,
-    );
+    if (_locationData == null) {
+      _currentPosition = CameraPosition(
+        target: LatLng(37.5665, 126.9780),
+        zoom: 10.0,
+      );
+    } else {
+      _currentPosition = CameraPosition(
+        target: LatLng(_locationData!.latitude!, _locationData!.longitude!),
+        zoom: 15.0,
+      );
+
+    }
   }
 
   Future<void> _searchHospital() async {
@@ -60,10 +66,8 @@ class _HospitalSearchPageState extends State<HospitalSearchPage> {
     String location = '${_locationData!.latitude},${_locationData!.longitude}';
     String type = 'hospital';
     String keyword = widget.recommendedSpecialistName;
-    String apiKey = 'YOUR_API_KEY'; // TODO: Google Places API 키 입력
-    Uri uri = Uri.parse(
-        '$apiUrl?location=$location&type=$type&keyword=$keyword&radius=5000&key=$apiKey');
-
+    String apiKey = 'AIzaSyBYQFpEdgAhhjvcsIqb8VrMkOMcwVYCAKs'; // 자신의 API 키로 변경
+    Uri uri = Uri.parse('$apiUrl?location=$location&type=$type&keyword=$keyword&radius=5000&key=$apiKey');
     http.Response response = await http.get(uri);
 
     if (response.statusCode == 200) {
@@ -92,18 +96,55 @@ class _HospitalSearchPageState extends State<HospitalSearchPage> {
           _markers.clear();
           _markers = markers;
         });
+        if (hospitals.isEmpty) {
+          _showNoResultDialog();
+        }
       } else {
-        print('API Error: ${jsonResponse['status']}');
+        _showErrorDialog(jsonResponse['status']);
       }
     } else {
-      print('HTTP Error: ${response.statusCode}');
+      _showErrorDialog('HTTP Error: ${response.statusCode}');
     }
   }
 
-  void _onMarkerTapped(MarkerId markerId) {
-    // TODO: 마커 탭 처리
+  void _showNoResultDialog() {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text('검색 결과 없음'),
+        content: Text('현재 위치에서 주변에 해당 진료과를 제공하는 병원이 없습니다. 일시적인 오류일 수도 있으니 다른 지도앱을 사용해보세요.'),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: Text('확인'),
+          ),
+        ],
+      ),
+    );
   }
 
+  void _showErrorDialog(String errorMessage) {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text('검색 실패'),
+        content: Text(errorMessage),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: Text('확인'),
+          ),
+        ],
+      ),
+    );
+  }
+
+
+  void _onMarkerTapped(MarkerId markerId) {}
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -112,7 +153,10 @@ class _HospitalSearchPageState extends State<HospitalSearchPage> {
       ),
       body: GoogleMap(
         mapType: MapType.normal,
-        initialCameraPosition: _currentPosition!,
+        initialCameraPosition: _currentPosition ?? CameraPosition(
+          target: LatLng(37.5665, 126.9780),
+          zoom: 10.0,
+        ),
         onMapCreated: (GoogleMapController controller) {
           _controller.complete(controller);
         },
@@ -129,7 +173,15 @@ class _HospitalSearchPageState extends State<HospitalSearchPage> {
           SizedBox(height: 16.0),
           FloatingActionButton(
             onPressed: () {
-              Navigator.pop(context); // 현재 페이지를 스택에서 제거하고 이전 페이지로 이동
+              _getLocation();
+            },
+            backgroundColor: Colors.blue,
+            child: Icon(Icons.my_location),
+          ),
+          SizedBox(height: 16.0),
+          FloatingActionButton(
+            onPressed: () {
+              Navigator.pop(context);
             },
             child: Icon(Icons.arrow_back),
           ),
