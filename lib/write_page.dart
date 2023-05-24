4b507f03e9ef9fda712649c46d66c14737365009
Recyclingbottle <hoholeo382@gmail.com>
2023-05-24 13:23:18 +0900

    0524, 건강일지 작성 페이지 프론트 구현

--- lib/write_page.dart
+++ lib/write_page.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:location/location.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'hospital_search.dart'; // 이전에 작성한 HospitalSearchPage 코드 임포트
+import 'package:intl/intl.dart';
 
 class WritePage extends StatefulWidget {
   const WritePage({Key? key}) : super(key: key);
@@ -11,26 +9,139 @@ class WritePage extends StatefulWidget {
 }
 
 class _WritePageState extends State<WritePage> {
+  DateTime? selectedDate;
+  String? summary;
+  String? details;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('건강일지 작성'),
+        backgroundColor: Color(0xff5CCFD4),
+        title: Text(
+          '건강일지 작성',
+          style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        ),
+        centerTitle: true,
       ),
       body: Container(
-        // 건강일지 작성 페이지의 내용을 작성
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) => HospitalSearchPage(recommendedSpecialistName: '학교'),
+        padding: EdgeInsets.all(10.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                showDatePicker(
+                  context: context,
+                  initialDate: DateTime.now(),
+                  firstDate: DateTime(2020),
+                  lastDate: DateTime(2025),
+                ).then((date) {
+                  if (date != null) {
+                    setState(() {
+                      selectedDate = date;
+                    });
+                  }
+                });
+              },
+              style: ElevatedButton.styleFrom(
+                primary: Color(0xff5CCFD4),
+              ),
+              child: Text(
+                selectedDate != null
+                    ? DateFormat('yyyy-MM-dd').format(selectedDate!)
+                    : '날짜 선택',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
             ),
-          );
-        },
-        child: const Icon(Icons.map),
+            SizedBox(height: 10.0),
+            TextField(
+              decoration: InputDecoration(
+                labelText: '요약',
+                labelStyle: TextStyle(
+                  color: Color(0xff5CCFD4),
+                ),
+              ),
+              style: TextStyle(
+                color: Color(0xff5CCFD4),
+              ),
+              onChanged: (value) {
+                setState(() {
+                  summary = value;
+                });
+              },
+            ),
+            SizedBox(height: 10.0),
+            TextField(
+              decoration: InputDecoration(
+                labelText: '상세 내용',
+                labelStyle: TextStyle(
+                  color: Color(0xff5CCFD4),
+                ),
+              ),
+              style: TextStyle(
+                color: Color(0xff5CCFD4),
+              ),
+              onChanged: (value) {
+                setState(() {
+                  details = value;
+                });
+              },
+            ),
+            SizedBox(height: 20.0),
+            ElevatedButton(
+              onPressed: () {
+                if (selectedDate != null && summary != null && details != null) {
+                  saveHealthRecord(
+                    DateFormat('yyyy-MM-dd').format(selectedDate!),
+                    summary!,
+                    details!,
+                  );
+                  Navigator.pop(context); // 페이지 닫기
+                } else {
+                  showDialog(
+                    context: context,
+                    builder: (context) => AlertDialog(
+                      title: Text('오류'),
+                      content: Text('모든 필드를 입력해주세요.'),
+                      actions: <Widget>[
+                        TextButton(
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                          child: Text('확인'),
+                        ),
+                      ],
+                    ),
+                  );
+                }
+              },
+              style: ElevatedButton.styleFrom(
+                primary: Color(0xff5CCFD4),
+              ),
+              child: Text(
+                '저장',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
+  }
+
+  void saveHealthRecord(String date, String summary, String details) {
+    print('저장된 건강 일지:');
+    print('날짜: $date');
+    print('요약: $summary');
+    print('상세 내용: $details');
   }
 }
